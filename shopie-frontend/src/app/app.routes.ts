d852a6704aa3d5cb33fc29763131f61b5270c91b
Isaac-blip98 <isaacndirangu216@gmail.com>
2025-06-26 11:46:03 +0300

    added a landing page

--- shopie-frontend/src/app/app.routes.ts
+++ shopie-frontend/src/app/app.routes.ts
@@ -1,32 +1,36 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'auth/register', pathMatch: 'full' },
+  { path: '', redirectTo: 'landing', pathMatch: 'full' },
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
   },
-{
-  path: 'admin',
-  loadChildren: () =>
-    import('./admin/admin.module').then((m) => m.AdminModule),
-},
   {
-  path: 'shop',
-  loadComponent: () =>
-    import('./shop/shop.component').then((m) => m.ShopComponent),
-},
-{
-  path: 'reset-password',
-  loadComponent: () =>
-    import('./auth/reset-password/reset-password.component').then(
-      (m) => m.ResetPasswordComponent
-    ),
-},
-{
-  path: 'cart',
-  loadComponent: () =>
-    import('./shop/cart/cart.component').then((m) => m.CartComponent),
-}
-
+    path: 'admin',
+    loadChildren: () =>
+      import('./admin/admin.module').then((m) => m.AdminModule),
+  },
+  {
+    path: 'shop',
+    loadComponent: () =>
+      import('./shop/shop.component').then((m) => m.ShopComponent),
+  },
+  {
+    path: 'reset-password',
+    loadComponent: () =>
+      import('./auth/reset-password/reset-password.component').then(
+        (m) => m.ResetPasswordComponent
+      ),
+  },
+  {
+    path: 'cart',
+    loadComponent: () =>
+      import('./shop/cart/cart.component').then((m) => m.CartComponent),
+  },
+  {
+    path: 'landing',
+    loadComponent: () =>
+      import('./Landing/landing.component').then((m) => m.LandingComponent),
+  },
 ];
